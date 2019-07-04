ac8f82629fd734ad1883886cd6a2f7230c76309a
c <cvaldez.issc@gmail.com>
2019-07-04 11:15:04 -0500

    Connecting to the $PAC blockchain
    Includes the generation of the checkpoints provided by ./build-checkpoints script, changed the destination java version and some aditional configurations part 1

--- core/src/main/java/org/bitcoinj/core/CoinDefinition.java
+++ core/src/main/java/org/bitcoinj/core/CoinDefinition.java
@@ -12,9 +12,9 @@ import java.util.Map;
 public class CoinDefinition {
 
 
-    public static final String coinName = "Dash";
-    public static final String coinTicker = "DASH";
-    public static final String coinURIScheme = "dash";
+    public static final String coinName = "Pac";
+    public static final String coinTicker = "$PAC";
+    public static final String coinURIScheme = "paccoin";
     public static final String cryptsyMarketId = "155";
     public static final String cryptsyMarketCurrency = "BTC";
     public static final String PATTERN_PRIVATE_KEY_START_UNCOMPRESSED = "[7]";
@@ -41,7 +41,7 @@ public class CoinDefinition {
     public static final String BLOCKEXPLORER_BLOCK_PATH = "block/";                 //blockr.io path
     public static final String BLOCKEXPLORER_BASE_URL_TEST = "http://test.explorer.dash.org/";
 
-    public static final String DONATION_ADDRESS = "Xdeh9YTLNtci5zSL4DDayRSVTLf299n9jv";  //Hash Engineering donation DASH address
+    public static final String DONATION_ADDRESS = "PEFkgGQYCM3Zxa9QcVAWnC3cjAzfmbfkYT";  //Hash Engineering donation DASH address
 
     enum CoinHash {
         SHA256,
@@ -79,8 +79,8 @@ public class CoinDefinition {
     //
     // Dash 0.12.1.x
     //
-    public static final int PROTOCOL_VERSION = 70210;          //version.h PROTOCOL_VERSION
-    public static final int MIN_PROTOCOL_VERSION = 70209;        //version.h MIN_PROTO_VERSION
+    public static final int PROTOCOL_VERSION = 70215;          //version.h PROTOCOL_VERSION
+    public static final int MIN_PROTOCOL_VERSION = 70215;        //version.h MIN_PROTO_VERSION
 
     public static final int BLOCK_CURRENTVERSION = 2;   //CBlock::CURRENT_VERSION
     public static final int MAX_BLOCK_SIZE = 1 * 1000 * 1000;
@@ -88,34 +88,34 @@ public class CoinDefinition {
 
     public static final boolean supportsBloomFiltering = true; //Requires PROTOCOL_VERSION 70000 in the client
 
-    public static final int Port    = 9999;       //protocol.h GetDefaultPort(testnet=false)
-    public static final int TestPort = 19999;     //protocol.h GetDefaultPort(testnet=true)
+    public static final int Port    = 7112;       //protocol.h GetDefaultPort(testnet=false)
+    public static final int TestPort = 17112;     //protocol.h GetDefaultPort(testnet=true)
 
     //
     //  Production
     //
-    public static final int AddressHeader = 76;             //base58.h CBitcoinAddress::PUBKEY_ADDRESS
-    public static final int p2shHeader = 16;             //base58.h CBitcoinAddress::SCRIPT_ADDRESS
+    public static final int AddressHeader = 55;             //base58.h CBitcoinAddress::PUBKEY_ADDRESS
+    public static final int p2shHeader = 10;             //base58.h CBitcoinAddress::SCRIPT_ADDRESS
     public static final int dumpedPrivateKeyHeader = 128;   //common to all coins
     public static final long oldPacketMagic = 0xfbc0b6db;      //0xfb, 0xc0, 0xb6, 0xdb
-    public static final long PacketMagic = 0xbf0c6bbd;
+    public static final long PacketMagic = 0xc8e5612c;
 
     //Genesis Block Information from main.cpp: LoadBlockIndex
     static public long genesisBlockDifficultyTarget = (0x1e0ffff0L);         //main.cpp: LoadBlockIndex
-    static public long genesisBlockTime = 1390095618L;                       //main.cpp: LoadBlockIndex
-    static public long genesisBlockNonce = (28917698);                         //main.cpp: LoadBlockIndex
-    static public String genesisHash = "00000ffd590b1485b3caadc19b22e6379c733355108f107a430458cdf3407ab6"; //main.cpp: hashGenesisBlock
-    static public String genesisMerkleRoot = "e0028eb9648db56b1ac77cf090b99048a8007e2bb64b68f092c03c7f56a662c7";
+    static public long genesisBlockTime = 1517541873L;                       //main.cpp: LoadBlockIndex
+    static public long genesisBlockNonce = (340503L);                         //main.cpp: LoadBlockIndex
+    static public String genesisHash = "00000354655ff039a51273fe61d3b493bd2897fe6c16f732dbc4ae19f04b789e"; //main.cpp: hashGenesisBlock
+    static public String genesisMerkleRoot = "f3939e4de05e537431ef32bbebd9bc7c4e701f7205d72016f04686557af58dbf";
     static public int genesisBlockValue = 50;                                                              //main.cpp: LoadBlockIndex
     //taken from the raw data of the block explorer
-    static public String genesisTxInBytes = "04ffff001d01044c5957697265642030392f4a616e2f3230313420546865204772616e64204578706572696d656e7420476f6573204c6976653a204f76657273746f636b2e636f6d204973204e6f7720416363657074696e6720426974636f696e73";   //"limecoin se convertira en una de las monedas mas segura del mercado, checa nuestros avances"
-    static public String genesisTxOutBytes = "040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9";
+    static public String genesisTxInBytes = "04ffff001d01044c57426974636f696e20426c6f636b20233530373230323a2030303030303030303030303030303030303032646366383861643766656365383639346361663735313234626131383165323963653338633736623763333738";   //"limecoin se convertira en una de las monedas mas segura del mercado, checa nuestros avances"
+    static public String genesisTxOutBytes = "0411345e927d2d3abb85541e23b211f5b9019f2b240fb9bd4b1c44234993639293846cfc74154d293a3bf7ba74592f5f358127c0062a621d3b153089d0f5bb84e5";
 
     //net.cpp strDNSSeed
     static public String[] dnsSeeds = new String[] {
-            "dnsseed.dash.org",
-            "dnsseed.dashdot.io",
-            "dnsseed.masternode.io",
+            "dns-seeder-next.paccoin.io",
+            "dns-seeder-foxtrot.paccoin.io",
+            "dnsseed2.paccoin.io"
     };
 
 
@@ -128,10 +128,10 @@ public class CoinDefinition {
     public static final int testnetAddressHeader = 140;             //base58.h CBitcoinAddress::PUBKEY_ADDRESS_TEST
     public static final int testnetp2shHeader = 19;             //base58.h CBitcoinAddress::SCRIPT_ADDRESS_TEST
     public static final long testnetPacketMagic = 0xcee2caff;      //
-    public static final String testnetGenesisHash = "00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c";
+    public static final String testnetGenesisHash = "00000da63bd9478b655ef6bf1bf76cd9af05202ab68643f9091e049b2b5280ed";
     static public long testnetGenesisBlockDifficultyTarget = (0x1e0ffff0L);         //main.cpp: LoadBlockIndex
-    static public long testnetGenesisBlockTime = 1390666206L;                       //main.cpp: LoadBlockIndex
-    static public long testnetGenesisBlockNonce = (3861367235L);                         //main.cpp: LoadBlockIndex
+    static public long testnetGenesisBlockTime = 1517541975L;                       //main.cpp: LoadBlockIndex
+    static public long testnetGenesisBlockNonce = (2279587L);                         //main.cpp: LoadBlockIndex
 
 
 
@@ -170,19 +170,13 @@ public class CoinDefinition {
     public static void initCheckpoints(Map<Integer, Sha256Hash> checkpoints)
     {
 
-        checkpoints.put(  1500, Sha256Hash.wrap("000000aaf0300f59f49bc3e970bad15c11f961fe2347accffff19d96ec9778e3"));
-        checkpoints.put(  4991, Sha256Hash.wrap("000000003b01809551952460744d5dbb8fcbd6cbae3c220267bf7fa43f837367"));
-        checkpoints.put(  9918, Sha256Hash.wrap("00000000213e229f332c0ffbe34defdaa9e74de87f2d8d1f01af8d121c3c170b"));
-        checkpoints.put( 16912, Sha256Hash.wrap("00000000075c0d10371d55a60634da70f197548dbbfa4123e12abfcbc5738af9"));
-        checkpoints.put( 23912, Sha256Hash.wrap("0000000000335eac6703f3b1732ec8b2f89c3ba3a7889e5767b090556bb9a276"));
-        checkpoints.put( 35457, Sha256Hash.wrap("0000000000b0ae211be59b048df14820475ad0dd53b9ff83b010f71a77342d9f"));
-        checkpoints.put( 45479, Sha256Hash.wrap("000000000063d411655d590590e16960f15ceea4257122ac430c6fbe39fbf02d"));
-        checkpoints.put( 55895, Sha256Hash.wrap("0000000000ae4c53a43639a4ca027282f69da9c67ba951768a20415b6439a2d7"));
-        checkpoints.put( 68899, Sha256Hash.wrap("0000000000194ab4d3d9eeb1f2f792f21bb39ff767cb547fe977640f969d77b7"));
-        checkpoints.put( 74619, Sha256Hash.wrap("000000000011d28f38f05d01650a502cc3f4d0e793fbc26e2a2ca71f07dc3842"));
-        checkpoints.put( 75095, Sha256Hash.wrap("0000000000193d12f6ad352a9996ee58ef8bdc4946818a5fec5ce99c11b87f0d"));
-        checkpoints.put( 88805, Sha256Hash.wrap("00000000001392f1652e9bf45cd8bc79dc60fe935277cd11538565b4a94fa85f"));
-        checkpoints.put( 90544, Sha256Hash.wrap("000000000001b284b79a44a95215d7e6cf9e22cd4f9b562f2cc796e941e0e411"));
+        checkpoints.put(  100, Sha256Hash.wrap("000005065df82218140bc7c59cfd6913eaa5a88f3255ccc977c546cb0beb9ff0"));
+        checkpoints.put(  12096, Sha256Hash.wrap("0000000000000097702bbbfb8c00bb228dd1e2f95ae448fc504b35a46bfcd4fe"));
+        checkpoints.put(  15000, Sha256Hash.wrap("00000000000001d2b0bbeaabd957dc7a80ea6f8ecc39ec32f60c70391288893b"));
+        checkpoints.put( 20000, Sha256Hash.wrap("000000000000006abf889a12aa85fe3c713f2520e0af1c1adf2ef70df0fb3320"));
+        checkpoints.put( 21822, Sha256Hash.wrap("000000000000002d00501b6de7a9bd92c8cb2e4874a17bc3c817825539203555"));
+        checkpoints.put( 24005, Sha256Hash.wrap("000000000000030cc60b574b820f0651bd5850d8cfc438d8ed10d184d6de53ff"));
+        checkpoints.put( 29239, Sha256Hash.wrap("00000000000001c7cfaf4d57fb28e134a0aaf26ef994c6d8fc499c0141ed6ab2"));
     }
 
     //Unit Test Information
--- core/src/main/java/org/bitcoinj/core/NetworkParameters.java
+++ core/src/main/java/org/bitcoinj/core/NetworkParameters.java
@@ -640,11 +640,11 @@ public abstract class NetworkParameters {
     public abstract int getProtocolVersionNum(final ProtocolVersion version);
 
     public static enum ProtocolVersion {
-        MINIMUM(70213),
+        MINIMUM(70215),
         PONG(60001),
         BLOOM_FILTER(MINIMUM.getBitcoinProtocolVersion()),
-        DMN_LIST(70213),
-        CURRENT(70213);
+        DMN_LIST(70215),
+        CURRENT(70215);
 
         private final int bitcoinProtocol;
 
